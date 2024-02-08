01d7059ed4965f60a05735c197fa0a7be931a7c7
Dario Simpson <simpson.dario@gmail.com>
2024-02-08 16:26:50 -0500

    calling udp as server as well as client

--- reliableUI/ReliableUI.cpp
+++ reliableUI/ReliableUI.cpp
@@ -1,12 +1,6 @@
-//LET USER SELECT MODE
-	//CLIENT
-		//IF CLIENT SELECTED, ASK FOR ADITIONAL INFORMATION
-			//IP OF SERVER
-			//FILENAME
-	//SERVER
-//CALL UDP BASED ON MODE
 #include "Client.h"
 #include <iostream>
+#include <cstdlib> 
 
 
 bool getClientInformation(Client& client);
@@ -16,34 +10,6 @@ bool getClientInformation(Client& client);
 
 int main()
 {
-	//Client client;
-	//string ipAddress = " ";
-	//int port = 0;
-	//string filename = " ";
-
-
-	//cout << "What's the IP ADDRESS\n";
-	//cin >> ipAddress;
-
-	//cout << "What's the PORT\n";
-	//cin >> port;
-
-	//cout << "What's the FILENAME\n";
-	//cin >> filename;
-
-	//client.setIpAddress(ipAddress);
-	//client.setPort(port);
-	//client.setFilename(filename);
-
-
-	//select client or server
-	//if client is selected
-		//get values
-		//pass values to reliable udp
-	//if server is selected
-		//start reliable udp with no parameters
-
-
 	int choice;
 
 	// Display menu
@@ -61,35 +27,21 @@ int main()
 		{
 			return 0;
 		}
-		//get values from client class
-		//call server with values
+
+		//START UDP AS CLIENT
+		//string reliableUDP = "reliableUDP.exe " + client.getIpAddress() + " " + to_string(client.getPort()) + " " + client.getFilename();
+		//system(reliableUDP.c_str());
 	}
 	else if(choice == 2)
 	{
-		//start server
+		//START UDP AS SERVER
+		//string reliableUDP = "reliableUDP.exe";
+		//system(reliableUDP.c_str());
 	}
 	else
 	{
-		//error 
+		std::cout << "Invalid choice. Please select 1 or 2.\n";
 	}
-
-	// Process user choice using switch statement
-	//switch (choice) {
-	//case 1:
-	//	Client client;
-	//	bool retValue = getClientInformation(client);
-	//	if(!retValue)
-	//	{
-	//		break;
-	//	}
-	//	break;
-	//case 2:
-	//	//CALL RELIABLE UDP
-	//	break;
-	//default:
-	//	std::cout << "Invalid choice. Please select 1 or 2.\n";
-	//	break;
-	//}
 
 	return 0;
 }
@@ -113,8 +65,6 @@ bool getClientInformation(Client& client)
 	cout << "What's the FILENAME\n";
 	cin >> filename;
 
-	//RETURN VALUE IS BOOL
-	//CHECK FOR ERRORS AND DISPLAY
 	bool isIpCorrect = client.setIpAddress(ipAddress);
 	bool isPortCorrect = client.setPort(port);
 	bool isFilenameCorrect = client.setFilename(filename);

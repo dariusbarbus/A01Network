38b2722f7f2d315e8bd92e1ba17e5addf9df177c
Dario Simpson <simpson.dario@gmail.com>
2024-02-08 16:18:31 -0500

    main file calling client class and assigning values, missing calling the server with those values

--- reliableUI/Client.h
+++ reliableUI/Client.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <regex>
 using namespace std;
+
+#define MIN_PORT 1
+#define MAX_PORT 65535
+
 
 class Client{
 private:
@@ -10,33 +15,90 @@ private:
     string filename;
 
 public:
-    //CONSTRUCTOR
-    Client();
-	Client(string ipAddress, int port, string filename);
+    Client()
+    {
+        ipAddress = "";
+        port = 1;
+        filename = "";
+    }
+
+    string getIpAddress()
+    {
+        return ipAddress;
+    }
+
+    int getPort()
+    {
+        return port;
+    }
+
+    string getFilename()
+    {
+        return filename;
+    }
 
 
-    //GETTERS
-    string getIpAddress();
-    int getPort();
-    string getFilename();
 
-	//SETTERS
-    void setIpAddress(string ipAddress);
-    void setPort(int port);
-    void setFilename(string filename);
+    bool setIpAddress(string ipAddress)
+    {
+        bool retValue = isIpAddressValid(ipAddress);
+        if (!retValue)
+        {
+            return false;
+        }
+        this->ipAddress = ipAddress;
+        return true;
+    }
 
-    //CHECKER
-    bool isIpAddressValid(string& ip);
+    bool setPort(int port)
+    {
+        bool retValue = isPortValid(port);
+        if (!retValue)
+        {
+            return false;
+        }
+        this->port = port;
+        return true;
+    }
+
+    bool setFilename(string filename)
+    {
+        bool retValue = isFilenameValid(filename);
+        if (!retValue)
+        {
+            return false;
+        }
+        this->filename = filename;
+        return true;
+    }
 
 
-    void printData();
-    //void getInput();
 
-    //string getIPAddress();
+    bool isIpAddressValid(string& ip)
+    {
+        std::regex ipRegex("^\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b$");
+        return std::regex_match(ip, ipRegex);
+    }
 
-    //int getPort();
 
-    //string getFilename();
+    bool isPortValid(int port)
+    {
+        if (port >= MIN_PORT && port <= MAX_PORT)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    bool isFilenameValid(string& file)
+    {
+        std::regex filenameRegex("^[^.]+\\.[^.]+$");    //checks for an extension
+        return std::regex_match(file, filenameRegex);
+    }
+
+
 };
-
-
--- reliableUI/ReliableUI.cpp
+++ reliableUI/ReliableUI.cpp
@@ -9,9 +9,96 @@
 #include <iostream>
 
 
+bool getClientInformation(Client& client);
+
+
+
+
 int main()
 {
 	//Client client;
+	//string ipAddress = " ";
+	//int port = 0;
+	//string filename = " ";
+
+
+	//cout << "What's the IP ADDRESS\n";
+	//cin >> ipAddress;
+
+	//cout << "What's the PORT\n";
+	//cin >> port;
+
+	//cout << "What's the FILENAME\n";
+	//cin >> filename;
+
+	//client.setIpAddress(ipAddress);
+	//client.setPort(port);
+	//client.setFilename(filename);
+
+
+	//select client or server
+	//if client is selected
+		//get values
+		//pass values to reliable udp
+	//if server is selected
+		//start reliable udp with no parameters
+
+
+	int choice;
+
+	// Display menu
+	std::cout << "Select an option:\n";
+	std::cout << "1. Client\n";
+	std::cout << "2. Server\n";
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+
+	if(choice == 1)
+	{
+		Client client;
+		bool retValue = getClientInformation(client);
+		if(!retValue)
+		{
+			return 0;
+		}
+		//get values from client class
+		//call server with values
+	}
+	else if(choice == 2)
+	{
+		//start server
+	}
+	else
+	{
+		//error 
+	}
+
+	// Process user choice using switch statement
+	//switch (choice) {
+	//case 1:
+	//	Client client;
+	//	bool retValue = getClientInformation(client);
+	//	if(!retValue)
+	//	{
+	//		break;
+	//	}
+	//	break;
+	//case 2:
+	//	//CALL RELIABLE UDP
+	//	break;
+	//default:
+	//	std::cout << "Invalid choice. Please select 1 or 2.\n";
+	//	break;
+	//}
+
+	return 0;
+}
+
+
+
+
+bool getClientInformation(Client& client)
+{
 	string ipAddress = " ";
 	int port = 0;
 	string filename = " ";
@@ -26,12 +113,15 @@ int main()
 	cout << "What's the FILENAME\n";
 	cin >> filename;
 
+	//RETURN VALUE IS BOOL
+	//CHECK FOR ERRORS AND DISPLAY
+	bool isIpCorrect = client.setIpAddress(ipAddress);
+	bool isPortCorrect = client.setPort(port);
+	bool isFilenameCorrect = client.setFilename(filename);
 
-
-	Client client(ipAddress, port, filename);
-	cout << client.getIpAddress() << endl;
-	cout << client.getPort() << endl;
-	cout << client.getFilename() << endl;
-
-	return 0;
+	if(!isIpCorrect || !isPortCorrect || !isFilenameCorrect)
+	{
+		return false;
+	}
+	return true;
 }
